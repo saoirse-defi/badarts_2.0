97abaf48f6bab054457d0be630f9357cacf5e571
saoirse-defi <saoirsef@yahoo.co.uk>
2022-04-05 13:15:10 +0000

    remove bag, profile

--- badarts/badarts/settings.py
+++ badarts/badarts/settings.py
@@ -83,7 +83,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'bag.contexts.bag_contents',
                 'products.contexts.category_context',
             ],
         },
