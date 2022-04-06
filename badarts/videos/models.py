91652e347fc90657a5e6d02e9096a2ed2dbcb210
saoirse-defi <saoirsef@yahoo.co.uk>
2022-04-06 11:22:33 +0000

    added artist photos, navbar logo

--- badarts/videos/models.py
+++ badarts/videos/models.py
@@ -1,12 +1,19 @@
 import uuid
 from django.db import models
 
+from homepage.models import Artist
+
 
 class Video(models.Model):
     id = models.UUIDField(primary_key=True,
                           default=uuid.uuid4,
                           editable=False)
     name = models.CharField(max_length=254)
+    artist = models.ForeignKey(Artist,
+                               null=False,
+                               blank=False,
+                               default=None,
+                               on_delete=models.CASCADE)
     thumbnail_url = models.CharField(max_length=254)
     youtube_url = models.CharField(max_length=254)
     data_caption = models.CharField(max_length=254)
