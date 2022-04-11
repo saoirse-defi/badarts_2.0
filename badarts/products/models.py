1c531e578059e2e35a8a59dac86f5f0f88414caa
saoirse-defi <saoirsef@yahoo.co.uk>
2022-04-11 12:57:14 +0000

    fix carousel, add out of stock to product

--- badarts/products/models.py
+++ badarts/products/models.py
@@ -33,6 +33,7 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     active = models.BooleanField(default=True, null=False, blank=False)
+    in_stock = models.BooleanField(default=True, null=False, blank=False)
 
     def __str__(self):
         return str(self.name)
