f32e976f7ba34aa9d289bfd75804b562a6af66e7
saoirse-defi <saoirsef@yahoo.co.uk>
2022-04-13 17:07:34 +0000

    center event title, add padding

--- badarts/homepage/admin.py
+++ badarts/homepage/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Message, Release, Artist, Event, Photo
+from .models import Release, Artist, Event, Photo
 
 # Register your models here.
-admin.site.register(Message)
 admin.site.register(Release)
 admin.site.register(Artist)
 admin.site.register(Event)
