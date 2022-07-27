0da831efe4e0fa0e1ab7cb1cae61d8e548877ddb
saoirse-defi <saoirsef@yahoo.co.uk>
2022-07-27 15:10:15 +0100

    remove static root

--- badarts/badarts/settings.py
+++ badarts/badarts/settings.py
@@ -167,7 +167,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join('staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join('static'), )
 
